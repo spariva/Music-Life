1701a2ad2e840c357b472e07f21dcdd056d0b030
JuanMiguelLeon <91685178+JuanMiguelLeon@users.noreply.github.com>
2023-10-10 22:28:12 +0200

    Create login.js
    Also created directory js

---
+++ public/js/login.js
@@ -0,0 +1,11 @@
+let crearCuenta = document.querySelector('#crearCuenta');
+let conectarCuenta = document.querySelector('#conectarCuenta');
+let body = document.querySelector('body');
+
+crearCuenta.onclick = function(){
+    body.classList.add('crearCuenta');
+}
+
+conectarCuenta.onclick = function(){
+    body.classList.remove('crearCuenta');
+}
