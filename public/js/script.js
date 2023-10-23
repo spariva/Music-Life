ab804f5d7de07ec664d85c71619d1ccb24130b7a
Sergio :) <sergio.serrano3@educa.madrid.org>
2023-10-24 00:11:26 +0200

    nuevos videos fondo

--- public/js/script.js
+++ public/js/script.js
@@ -16,9 +16,21 @@ document.addEventListener('mousemove', function (e) {
     });
 });
 
-function modoOscuro() {
-    var textoHTML = document.body;
-    var textoCabecera = document.getElementById('header');
-    textoHTML.style.color = 'white';
-    textoCabecera.style.color = 'white';
-}
+// function modoOscuro() {
+//     // La idea es que cambie el video a modo oscruo
+//     var videoElement = document.getElementById('videoFondo');
+
+//     // Verifica si el video actual contiene 'FondoIndexClaro.mp4' en su ruta
+//     if (videoElement.src.includes('FondoIndexClaro.mp4')) {
+//         // Cambia el video a FondoIndexOscuro.mp4 si el modo claro está activo
+//         videoElement.src = '../img/FondoIndexOscuro.mp4';
+//     } else {
+//         // Cambia el video a FondoIndexClaro.mp4 si el modo oscuro está activo
+//         videoElement.src = '../img/FondoIndexClaro.mp4';
+//     }
+
+//     // var textoHTML = document.body;
+//     // var textoCabecera = document.getElementById('header');
+//     // textoHTML.style.color = 'white';
+//     // textoCabecera.style.color = 'white';
+// }
