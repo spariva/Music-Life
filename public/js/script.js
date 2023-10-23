2f6c268468ac624a4357109750163d65121214c8
Sergio :) <sergio.serrano3@educa.madrid.org>
2023-10-24 01:19:06 +0200

    fondos ya completos y comienzo modo oscuro

--- public/js/script.js
+++ public/js/script.js
@@ -16,21 +16,39 @@ document.addEventListener('mousemove', function (e) {
     });
 });
 
-// function modoOscuro() {
-//     // La idea es que cambie el video a modo oscruo
-//     var videoElement = document.getElementById('videoFondo');
+function modoOscuro() {
+    // La idea es que cambie el video y texto a modo oscuro o claro 
+    var videoElement = document.getElementById('videoFondo');
+    var textoBotonModoOscuro = document.getElementById("modo-oscuro");
+    var elementosNavbar = document.getElementsByClassName('textoCabecera');
+    var textoCabecera = document.getElementById('logo');
+    // var navbar = document.querySelector('.navbar'); 
 
-//     // Verifica si el video actual contiene 'FondoIndexClaro.mp4' en su ruta
-//     if (videoElement.src.includes('FondoIndexClaro.mp4')) {
-//         // Cambia el video a FondoIndexOscuro.mp4 si el modo claro está activo
-//         videoElement.src = '../img/FondoIndexOscuro.mp4';
-//     } else {
-//         // Cambia el video a FondoIndexClaro.mp4 si el modo oscuro está activo
-//         videoElement.src = '../img/FondoIndexClaro.mp4';
-//     }
+    // Verifica si el video actual contiene 'FondoIndexClaro.mp4' en su ruta
+    if (videoElement.src.includes('FondoIndexClaro.mp4')) {
+        // Cambia el video a FondoIndexOscuro.mp4 si el modo claro está activo
+        videoElement.src = '../img/FondoIndexOscuro.mp4';
+        textoBotonModoOscuro.textContent = "Modo Claro";
 
-//     // var textoHTML = document.body;
-//     // var textoCabecera = document.getElementById('header');
-//     // textoHTML.style.color = 'white';
-//     // textoCabecera.style.color = 'white';
-// }
+        // navbar.style.backgroundColor = 'white';
+
+        for (var i = 0; i < elementosNavbar.length; i++) {
+            elementosNavbar[i].style.color = 'white';
+        }
+        textoCabecera.style.color = 'white';
+    } else {
+        // Cambia el video a FondoIndexClaro.mp4 si el modo oscuro está activo
+        videoElement.src = '../img/FondoIndexClaro.mp4';
+        textoBotonModoOscuro.textContent = "Modo Oscuro";
+
+        // navbar.style.backgroundColor = 'black';
+
+        for (var i = 0; i < elementosNavbar.length; i++) {
+            elementosNavbar[i].style.color = 'black';
+        }
+        textoCabecera.style.color = 'black';
+    }
+
+
+}
+
