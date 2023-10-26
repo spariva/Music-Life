4533117be3dc12d7b6585b014a134a9c7843eafb
Sergio :) <sergio.serrano3@educa.madrid.org>
2023-10-26 18:43:30 +0200

    contacto form

--- public/js/contacto.js
+++ public/js/contacto.js
@@ -22,29 +22,22 @@ function modoOscuro() {
     var textoBotonModoOscuro = document.getElementById("modo-oscuro");
     var elementosNavbar = document.getElementsByClassName('textoCabecera');
     var textoCabecera = document.getElementById('logo');
-    var textoSpotify = document.getElementsByClassName('textoSpotify');
-    var formaSpotify = document.getElementsByClassName('formaSpotify');
-
-    // TODO: esta clase solo se implementa modo oscuro aqui falta en el index y mas abajo se cambia color
-    //var textoContenido = document.getElementsById('cabecera');
-    // var navbar = document.querySelector('.navbar'); 
+    var textoContacto = document.getElementsByClassName('textoContacto');
+    // var formaSpotify = document.getElementsByClassName('formaSpotify');
 
     // Verifica si el video actual contiene 'FondoIndexClaro.mp4' en su ruta
-    if (videoElement.src.includes('FondoSpotifyClaro.mp4')) {
+    if (videoElement.src.includes('FondoIndexClaro.mp4')) {
         // Cambia el video a FondoIndexOscuro.mp4 si el modo claro está activo
-        videoElement.src = '../img/FondoSpotifyOscuro.mp4';
+        videoElement.src = '../img/FondoIndexOscuro.mp4';
         textoBotonModoOscuro.textContent = "Modo Claro";
-        //textoContenido.style.color='white';
 
-        // navbar.style.backgroundColor = 'white';
-
-        for (var i = 0; i < textoSpotify.length; i++) {
-            textoSpotify[i].style.color = 'white';
+        for (var i = 0; i < textoContacto.length; i++) {
+         textoContacto[i].style.color = 'white';
         }
 
-        for (var i = 0; i < formaSpotify.length; i++) {
-            formaSpotify[i].style.backgroundColor = 'rgba(0, 0, 0, 0.5)';
-        }
+        // for (var i = 0; i < formaSpotify.length; i++) {
+        //     formaSpotify[i].style.backgroundColor = 'rgba(0, 0, 0, 0.5)';
+        // }
 
         for (var i = 0; i < elementosNavbar.length; i++) {
             elementosNavbar[i].style.color = 'white';
@@ -52,19 +45,19 @@ function modoOscuro() {
         textoCabecera.style.color = 'white';
     } else {
         // Cambia el video a FondoIndexClaro.mp4 si el modo oscuro está activo
-        videoElement.src = '../img/FondoSpotifyClaro.mp4';
+        videoElement.src = '../img/FondoIndexClaro.mp4';
         textoBotonModoOscuro.textContent = "Modo Oscuro";
 
         // navbar.style.backgroundColor = 'black';
         //textoContenido.style.color='black';
 
-        for (var i = 0; i < textoSpotify.length; i++) {
-            textoSpotify[i].style.color = 'black';
+        for (var i = 0; i < textoContacto.length; i++) {
+             textoContacto[i].style.color = 'black';
         }
 
-        for (var i = 0; i < formaSpotify.length; i++) {
-            formaSpotify[i].style.backgroundColor = 'rgba(255, 255, 255, 0.5)';
-        }
+        // for (var i = 0; i < formaSpotify.length; i++) {
+        //     formaSpotify[i].style.backgroundColor = 'rgba(255, 255, 255, 0.5)';
+        // }
 
         for (var i = 0; i < elementosNavbar.length; i++) {
             elementosNavbar[i].style.color = 'black';
