8d6bba3c4401dc496dc7fec029ae1ced1a0980eb
JuanMiguelLeon <91685178+JuanMiguelLeon@users.noreply.github.com>
2023-10-27 08:04:38 +0200

    Add files via upload

--- public/js/usuario.js
+++ public/js/usuario.js
@@ -1,14 +1,22 @@
 //Colores aleatorios RGB
 function generarColorAleatorio() {
-    const r = Math.floor(Math.random() * 256);
-    const g = Math.floor(Math.random() * 256);
-    const b = Math.floor(Math.random() * 256);
-    return `rgb(${r}, ${g}, ${b})`;
+    const RED = Math.floor(Math.random() * 256);
+    const GREEN = Math.floor(Math.random() * 256);
+    const BLUE = Math.floor(Math.random() * 256);
+
+    return `rgb(${RED}, ${GREEN}, ${BLUE})`;
+}
+
+function gradosColorAleatorio(){
+    const GRADOS = Math.floor(Math.random() * 361);
+    return `${GRADOS}deg`;
 }
 
 //Colores aleatorios para el bloque izquierdo
 const bloqueIzq = document.getElementById('menuUsuario__izquierda');
-bloqueIzq.style.background = `linear-gradient(45deg, ${generarColorAleatorio()}, ${generarColorAleatorio()})`;
+bloqueIzq.style.background = `linear-gradient(${gradosColorAleatorio()}, ${generarColorAleatorio()},
+                                                     ${generarColorAleatorio()},
+                                                     ${generarColorAleatorio()})`;
 
 //Colores aleatorios para el bloque derecho
 //const bloqueDrch = document.getElementById('menuUsuario__derecha');
