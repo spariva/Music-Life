68488b80857c8d20d3e6027ef8a37e8e8d44b847
Maki <makspariva@gmail.com>
2023-10-27 15:32:03 +0200

    Update login.js
    El login tiene el fondo verde para que vaya acorde con la estética.

--- public/js/login.js
+++ public/js/login.js
@@ -1,13 +1,16 @@
 let crearCuenta = document.querySelector('#crearCuenta');
 let conectarCuenta = document.querySelector('#conectarCuenta');
 let body = document.querySelector('body');
+let videoFondo = document.getElementById('videoFondo');
 
 crearCuenta.onclick = function(){
     body.classList.add('crearCuenta');
+    videoFondo.src = '../img/FondoSpotifyClaro.mp4';
 }
 
 conectarCuenta.onclick = function(){
     body.classList.remove('crearCuenta');
+    videoFondo.src = '../img/FondoIndexClaro.mp4';
 }
 
 
