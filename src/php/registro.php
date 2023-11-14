c52e780795b201ec7ab07b0c14e8fd0bd67ffa77
Maki Spariva Olona <110998002+spariva@users.noreply.github.com>
2023-11-14 21:42:51 +0100

    Cosas Php

---
+++ src/php/registro.php
@@ -0,0 +1,33 @@
+<?php
+include('../config/init.php');
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $usuario = $_POST["nombre_usuario"];
+    $email = $_POST["email"];
+    $contrasena = $_POST["contrasena"];
+
+
+    if (empty($usuario)) {
+        echo "El nombre de usuario es requerido.";
+    } 
+
+    if (empty($email)) {
+        echo "El correo electrónico es requerido.";
+    }
+    //la contrasena debería ser encriptada?
+    if (empty($contrasena)) {
+        echo "La contraseña es requerida.";
+    }
+
+    // Hacemos uso del singleton para obtener una instancia de la base de datos
+    $db = Database::getInstance();
+
+    $sql = "INSERT INTO usuarios (nombre, email, contrasena) VALUES (?, ?, ?)";
+    $stmt = $db->prepare($sql);
+    $stmt->execute([$usuario, $email, $contrasena]);
+
+    $db->cerrarConexion();
+
+    header("Location: ../public/html/usuario.html");
+    exit();
+}
