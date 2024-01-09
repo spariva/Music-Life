e3fed84173799ba0750324922e2bcff3998a88b1
Maki Spariva Olona <110998002+spariva@users.noreply.github.com>
2024-01-09 21:28:40 +0100

    Autoload Fixed =,)

--- public/login.php
+++ public/login.php
@@ -1,8 +1,7 @@
 <?php
-//include '../config/init.php';
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-session_start();
+require '../config/init.php';
+
+$a = new LoginManager('a', 'a');
 
 $errorsLogin = $_SESSION['errorsLogin'] ?? [];
 unset($_SESSION['errorsLogin']);
--- src/php/LoginManager.php
+++ src/php/LoginManager.php
@@ -1,5 +1,6 @@
 <?php
-// include_once '../config/init.php';
+
+include_once '../config/init.php';
 include_once './Sanitizer.php';
 class LoginManager{
     public $errors = [];
