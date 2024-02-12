28a20751b9e170a7ebcef141150fc9eda2e0c227
Sergio :) <sergio.serrano3@educa.madrid.org>
2024-02-12 14:03:17 +0100

    merge de cookies
    Merge a main de la rama cookies, ha habido conflicto en el idex.php pero ya esta resuelto

--- public/index.php
+++ public/index.php
@@ -34,18 +34,7 @@ if (isset($_COOKIE['aceptadas']) && $_COOKIE['aceptadas'] == true) {
 	</head>
 
 	<body>
-		<video src="./img/FondoIndexClaro.mp4" id="videoFondo" autoplay="true" muted="true" loop="true"
-			disablePictureInPicture></video>
-		<header id="header">
-			<nav class="navbar navbar-expand-lg navbar-light bg-light">
-				<div class="d-flex align-items-center">
-					<a class="textoCabecera" href="./index.php" id="logo">Music-Life</a>
 
-					<!-- desplegable para pantallas pequeñas -->
-					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
-						aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-						<span class="navbar-toggler-icon"></span>
-					</button>
 			<!--// Verificar si el usuario ingresó un nombre
 			if (nombrePlaylist !== null && nombrePlaylist !== "") {
 				// Obtener la URL del iframe
@@ -63,7 +52,7 @@ if (isset($_COOKIE['aceptadas']) && $_COOKIE['aceptadas'] == true) {
 						data: {
 							id_pl: matches, // Agrega el valor correcto para id_pl (puede ser vacío por ahora)
 							playlistName: nombrePlaylist,
-							creator: <?php echo $_SESSION['user']; ?> // Asumiendo que 'id_usuario' es la clave correcta para el ID del usuario
+							creator: < ?php echo $_SESSION['user']; ?> // Asumiendo que 'id_usuario' es la clave correcta para el ID del usuario
 						},
 						success: function(response) {
 							alert(response); // Muestra la respuesta del servidor (puedes personalizar el mensaje)
@@ -115,63 +104,6 @@ if (isset($_COOKIE['aceptadas']) && $_COOKIE['aceptadas'] == true) {
 			<iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/37i9dQZF1DX1LUyBs1uGpN?utm_source=generator" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
 			<iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/37i9dQZF1DZ06evO4e5iLu?utm_source=generator" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
 		</div>
-
-
-		<div id="restoPagina">
-			<div id="buscador">
-				<div id="lupaBuscador">
-					<div id="barraBusqueda" class="barraBusqueda">
-						<input type="text" id="nombrePlaylist" class="inputBuscador" placeholder="Introduzca la ruta embedida del álbum..." value="">
-						<button id="botonBuscar">Buscar</button>
-						<iframe id="iframeBusqueda" style="border-radius:12px" src="https://open.spotify.com/embed/playlist/6lHivMtxlldZdqEvpwGRxZ?utm_source=generator" width="100%" height="152" frameborder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
-					</div>
-				</div>
-
-				<div class="collapse navbar-collapse" id="navbarNav">
-					<ul class="navbar-nav ml-auto">
-						<li class="nav-item"><a class="nav-link" href="./login.php">Cuenta</a></li>
-						<li class="nav-item"><a class="nav-link" href="./usuario.php">Usuario</a></li>
-						<li class="nav-item"><a class="nav-link" href="./spotify.html">Spotify</a></li>
-						<li class="nav-item"><a class="nav-link" href="./contacto.php">Contacto</a></li>
-						<li class="nav-item"><a class="nav-link" href="https://github.com/spariva/Music-Life"
-								target="_blank">Info</a></li>
-						<li class="nav-item"><a class="nav-link" id="modo-oscuro">Modo Oscuro</a></li>
-					</ul>
-				</div>
-			</nav>
-		</header>
-
-
-		<div id="contenido">
-			<!-- https://open.spotify.com/embed/album/1pzvBxYgT6OVwJLtHkrdQK?utm_source=generator -->
-			<div class="contenedor" id="recomendado">
-				<div id="apartado">Top Artistas 2023</div>
-				<iframe style="border-radius:12px"
-					src="https://open.spotify.com/embed/playlist/37i9dQZF1DX5KpP2LN299J?utm_source=generator"
-					width="100%" height="152" frameBorder="0" allowfullscreen=""
-					allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
-					loading="lazy"></iframe>
-				<iframe style="border-radius:12px"
-					src="https://open.spotify.com/embed/playlist/37i9dQZF1DX2apWzyECwyZ?utm_source=generator"
-					width="100%" height="152" frameBorder="0" allowfullscreen=""
-					allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
-					loading="lazy"></iframe>
-				<iframe style="border-radius:12px"
-					src="https://open.spotify.com/embed/playlist/37i9dQZF1DX6bnzK9KPvrz?utm_source=generator"
-					width="100%" height="152" frameBorder="0" allowfullscreen=""
-					allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
-					loading="lazy"></iframe>
-				<iframe style="border-radius:12px"
-					src="https://open.spotify.com/embed/playlist/37i9dQZF1DX1LUyBs1uGpN?utm_source=generator"
-					width="100%" height="152" frameBorder="0" allowfullscreen=""
-					allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
-					loading="lazy"></iframe>
-				<iframe style="border-radius:12px"
-					src="https://open.spotify.com/embed/playlist/37i9dQZF1DZ06evO4e5iLu?utm_source=generator"
-					width="100%" height="152" frameBorder="0" allowfullscreen=""
-					allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
-					loading="lazy"></iframe>
-			</div>
 
 
 			<div id="restoPagina">
