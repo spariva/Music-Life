a60d26b416f5a6dcc1c69ff4c4b5f6d5ae4f59d9
Maki <makspariva@gmail.com>
2024-05-13 01:33:42 +0200

    Cleaning finished

--- src/API/spotify functions/1getTopTracks.js
+++ src/API/spotify functions/1getTopTracks.js
@@ -1,5 +1,5 @@
 // Authorization token that must have been created previously. See : https://developer.spotify.com/documentation/web-api/concepts/authorization o sea este token es el de mi cuenta, lo que tendría que ser es que el usuario se loguee con su cuenta y se cree un token para esa cuenta. 
-const token = 'BQDQIIrWo0NTCe98Ca86e9ndSRirfJps-3z8ownhfy-Bc8W-auCd6nqHd15sQA1SC6xVt2L1oWZUqRc2Os9PLdNMaV4-DnK947zFASGXzvDFMqubnwAOGj_x0i6kmhHMfLhIFwCwuNsx3aCkZI62m1-2TSus_0Kf1fYWtEaMFFPNaz63Cym-xsOu0_SEDRLhJlDUpULR6QVrfaxovZTCoUxty01ki0W9umgpjYXbxAYsfYCOjgM41F3P2AYr-7dxGQ';
+const token = '';
 async function fetchWebApi(endpoint, method, body) {
   const res = await fetch(`https://api.spotify.com/${endpoint}`, {
     headers: {
