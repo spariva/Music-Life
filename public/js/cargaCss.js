5672836169cacd184827485a39aca039bbe9e3a0
LuciaPrieto856 <luciia856@hotmail.es>
2024-06-02 18:31:20 +0200

    arreglado css login
    no cargaba bien porque cogía primero el de usuario en el condicional

--- public/js/cargaCss.js
+++ public/js/cargaCss.js
@@ -2,28 +2,31 @@
 let head = document.head;
 console.log(head);
 
+/**Se comprueba la url de la página que acabamos de cargar
+ * y se va añadiendo al head el css correspondiente
+ */
 if(window.location.href.includes('spotifyLab')){
     console.log('hola');
     head.innerHTML += `
     <link rel="stylesheet" type="text/css" href="./css/nuevocss.css">
     <link rel="stylesheet" type="text/css" href="./css/spotify-lab.css">
     `;
-    // head.appendChild(`<link rel="stylesheet" type="text/css" href="./css/nuevocss.css">`);
-    // head.appendChild(`<link rel="stylesheet" type="text/css" href="./css/navbar.css">`);
-    // head.appendChild(`<link rel="stylesheet" type="text/css" href="./css/star-rating.css">`);
-    // head.appendChild(`<link rel="stylesheet" type="text/css" href="./css/usuario.css">`);
 }else if(window.location.href.includes('index') || window.location.href === 'music-lifes.es'){
     head.innerHTML += `
     <link rel="stylesheet" type="text/css" href="./css/nuevocss.css">
     <link rel="stylesheet" type="text/css" href="./css/star-rating.css">
     `;
-}else if(window.location.href.includes('usuario')){
+}else if(window.location.href.includes('login')){
     head.innerHTML += `
-    <link rel="stylesheet" href="./css/usuario.css">
+    <link rel="stylesheet" href="./css/login.css">
     `;
 }else if(window.location.href.includes('login')){
     head.innerHTML += `
     <link rel="stylesheet" href="./css/login.css">
+    `;
+}else if(window.location.href.includes('usuario')){
+    head.innerHTML += `
+    <link rel="stylesheet" href="./css/usuario.css">
     `;
 }else if(window.location.href.includes('contacto')){
     head.innerHTML += `
